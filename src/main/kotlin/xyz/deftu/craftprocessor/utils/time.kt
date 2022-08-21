59e2771f6fd935d1cba0ba335ce13c4ae941eb05
Deftu <deftudev@gmail.com>
2022-08-21 19:06:32 +0200

    Fix the time formatting being wrong.

--- src/main/kotlin/xyz/deftu/craftprocessor/utils/time.kt
+++ src/main/kotlin/xyz/deftu/craftprocessor/utils/time.kt
@@ -13,10 +13,10 @@ fun Long.toFormattedTime(): String {
     val seconds = this / 1000 % 60
 
     var time = ""
-    if (weeks > 0) time += "$weeks week" + if (weeks > 1) "s" else "" + if (days > 0) ", " else ""
-    if (days > 0) time += "$days day" + if (days > 1) "s" else "" + if (hours > 0) ", " else ""
-    if (hours > 0) time += "$hours hour" + if (hours > 1) "s" else "" + if (minutes > 0) ", " else ""
-    if (minutes > 0) time += "$minutes minute" + if (minutes > 1) "s" else "" + if (seconds > 0) ", " else ""
+    if (weeks > 0) time += "$weeks week" + (if (weeks > 1) "s" else "") + (if (days > 0) ", " else "")
+    if (days > 0) time += "$days day" + (if (days > 1) "s" else "") + (if (hours > 0) ", " else "")
+    if (hours > 0) time += "$hours hour" + (if (hours > 1) "s" else "") + (if (minutes > 0) ", " else "")
+    if (minutes > 0) time += "$minutes minute" + (if (minutes > 1) "s" else "") + (if (seconds > 0) ", " else "")
     if (seconds > 0) time += "$seconds second" + if (seconds > 1) "s" else ""
     return time
 }
